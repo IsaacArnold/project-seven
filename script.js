1beefef337e2d6e4e66c805e7f49b1e06844c9f6
Isaac Arnold <isaacarnold@icloud.com>
2020-10-19 20:35:29 +1300

    Added line graph js

--- script.js
+++ script.js
@@ -17,3 +17,41 @@ alert.addEventListener('click', (e) => {
         alert.style.display = 'none';
     }
 });
+
+/* ====================
+Chart widgets
+==================== */
+
+// Creates outline for the line graph
+const trafficCanvas = document.getElementById('traffic-chart');
+let trafficData = {
+    labels: ['16-32', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rbga(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+};
+// Lets you set the chart options
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
+// Creates the chart itself
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData, 
+    options: trafficOptions
+});
